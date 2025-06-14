b8aad09436bc48f2fd345612dc0f7f4c60024abf
lokeshloki3 <lokesh1992p@gmail.com>
2025-06-14 21:48:20 +0530

    Routes created in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
+import Navbar from "./components/Navbar";
 
 function App() {
 
   return (
-    <>
-      <div className='text-red-900'>Hello</div>
-    </>
-  )
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
